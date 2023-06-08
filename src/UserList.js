2453c9144f197dda79b9bb8d46275db4e91fe5b9
Dina <chihaoui.dina@gmail.com>
2023-06-08 09:31:30 +0100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setUserList(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching user list:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {userList.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
